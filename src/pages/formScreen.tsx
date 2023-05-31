0ff53b450bfb0953800f6cb9e634cf7bd5965ae8
Adryan1928 <adryanoliveira1928@gmail.com>
2023-05-31 15:45:42 -0300

    está funcionando o formulário

--- src/pages/formScreen.tsx
+++ src/pages/formScreen.tsx
@@ -1,41 +1,61 @@
-import React from 'react';
-import { StyleSheet, View, Text, TextInput } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
 import * as yup from 'yup';
 import { Field, Form, Formik, useField } from 'formik';
 import { Input } from '@ui-kitten/components';
 
 const initialValues = {
-    email: 'okfofke',
+    email: '',
     senha: ''
 }
 
 export function FormScreen() {
     const schema = yup.object().shape({
-        email: yup.string().min(10, 'falta caractéries')
+        email: yup.string().min(10, 'falta caractéries').required('digite alguma coisa'),
+        senha: yup.string().required('digite alguma coisa').min(4, 'falta caractéries')
     })
     const name = {
         name: 'email'
     }
+
+    const [teste, setTeste] = useState('rkvorm');
+
+    const senha = useRef(null)
+    const email = useRef(null)
+
     // const [field, meta, helpers] = useField('email');
 
     return (
         <View  >
             <Text>Loguin</Text>
             <Formik
-                onSubmit={() => {}}
+                onSubmit={values => {console.log(values)}}
                 initialValues={initialValues}
                 validationSchema={schema}
-                validateOnChange
             >
-                {({ values, handleChange, handleBlur }) => (
-                    <View>
-                        <TextInput
-                            style={styles.input}
-                            onChangeText={handleChange('email')}
-                            onBlur={handleBlur('email')}
-                            value={values.email}
-                        />
-                    </View>
+                {({ values, handleChange, handleBlur, handleSubmit, errors }) => (
+                        <View>
+                            <Text>Digite seu email</Text>
+                            <TextInput
+                                ref={email}
+                                style={styles.input}
+                                onChangeText={handleChange('email')}
+                                onBlur={handleBlur('email')}
+                                value={values.email}
+                            />
+                            {errors.email && <Text style={{color: '#a71b1b'}} >{errors.email}</Text>}
+                            <Text>Digite sua senha</Text>
+                            <TextInput
+                                style={styles.input}
+                                ref={senha}
+                                onChangeText={handleChange('senha')}
+                                onBlur={handleBlur('senha')}
+                                value={values.senha}
+                            />
+                            {errors.senha && <Text style={{color: '#a71b1b'}} >{errors.senha}</Text>}
+                            <Button title='Enviar' onPress={() => handleSubmit()} />
+                            <Text>{teste}</Text>
+                        </View>
                 )}
             </Formik>
         </View>
